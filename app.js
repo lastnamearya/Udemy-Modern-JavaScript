505a30cf3af6314525170ffc86e49d9218555e39
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-12 18:29:23 +0530

    functions

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
-const color = "yellow";
+ // Function Declarations
 
-switch(color) {
-  case 'red': 
-    console.log('color is red');
-    break;
-  case 'blue':
-    console.log('color is blue');
-    break;
-  default:
-    console.log('color is not red or blue');
-    break;
+ function greet(firstName = 'John', lastName = 'Doe') {
+  //  ES5 Way
+  //  if(typeof firstName === 'undefined'){firstName = 'John'}
+  //  if(typeof lastName === 'undefined'){lastName = 'Doe'}
+
+  return 'hello ' + firstName + " " + lastName;
+ }
+
+ // console.log(greet());
+
+ // Immediately Invoked Function Expressions
+
+ (function(){
+   console.log('IIFE Ran...');
+ })();
+
+ (function(name){
+   console.log(`Hello ${name}`);
+ })('Brad');
+
+ const todo = {
+   add: function() {
+     console.log('Add todo..');
+   },
+   edit: function(id){
+    console.log(`Edit todo ${id}`); 
+   }
+ }
+
+// Add methods later to the object
+todo.delete = function(){
+  console.log('Delete todo...');
 }
 
-let day;
-
-switch(new Date().getDay()) {
-  case 0:
-    day = "sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 3:
-    day = "Tuesday";
-    break;
-  case 4:
-    day = "Wednesday";
-    break;
-  case 5:
-    day = "Thursday";
-    break;  
-  case 6:
-    day = "Friday";
-    break;
-  case 7:
-    day = "Saturday";
-    break;
-}
-
-console.log(`Today is ${day}`);
+ todo.add();
+ todo.edit(22);
+ todo.delete();
