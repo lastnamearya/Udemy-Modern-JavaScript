83cff521f2ba14480d0b056fd122aa3a42d473e9
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-12 23:58:21 +0530

    loops

--- app.js
+++ app.js
@@ -1,39 +1,76 @@
- // Function Declarations
+// For Loop
 
- function greet(firstName = 'John', lastName = 'Doe') {
-  //  ES5 Way
-  //  if(typeof firstName === 'undefined'){firstName = 'John'}
-  //  if(typeof lastName === 'undefined'){lastName = 'Doe'}
+for(let i = 0; i < 10; i++){
+  //console.log('Number ' + i);
+  if(i === 2){
+    console.log("2 is my favorite number");
+    continue;
+  }
 
-  return 'hello ' + firstName + " " + lastName;
- }
+  if(i === 5){
+    console.log("Stop the loop");
+    break;
+  }
 
- // console.log(greet());
-
- // Immediately Invoked Function Expressions
-
- (function(){
-   console.log('IIFE Ran...');
- })();
-
- (function(name){
-   console.log(`Hello ${name}`);
- })('Brad');
-
- const todo = {
-   add: function() {
-     console.log('Add todo..');
-   },
-   edit: function(id){
-    console.log(`Edit todo ${id}`); 
-   }
- }
-
-// Add methods later to the object
-todo.delete = function(){
-  console.log('Delete todo...');
+  console.log("Number " + i);
 }
 
- todo.add();
- todo.edit(22);
- todo.delete();
+// While Loop
+
+let i = 0;
+
+while(i < 10){
+  console.log('Number ' + i);
+  i++;
+} 
+
+// Do While
+
+let j = 0;
+
+do {
+  console.log('Number ' + j);
+  j++;
+}
+
+while(j < 10);
+
+const cars = ['Ford', 'Chevy', 'Honda', 'Toyota'];
+
+for(let i = 0; i < cars.length; i++) {
+  console.log(cars[i]);
+}
+
+// FOREACH Loop
+cars.forEach(function(car, index, array){
+  console.log(`${index} : ${car}`);
+  console.log(array);
+});
+
+ // Map  
+ const users = [
+   {id: 1, name: "John"},
+   {id: 2, name: 'Sara'},
+   {id: 3, name: 'Karen'},
+   {id: 4, name: 'Steve'}
+ ];
+
+ const ids = users.map(function(user){
+   return user.id;
+ });
+
+ console.log(ids);
+
+ // for in loop
+ 
+ const user = {
+   firstName: 'John',
+   lastName: 'Doe',
+   age: 40
+ }
+
+ for(let x in user){
+   console.log(`${x} : ${user[x]}`);
+ }
+
+
