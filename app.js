6651cadf28c3b0a908fd92204b20e2413d8b423a
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-12 17:59:10 +0530

    switch statement

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
- const id = 100;
+const color = "yellow";
 
+switch(color) {
+  case 'red': 
+    console.log('color is red');
+    break;
+  case 'blue':
+    console.log('color is blue');
+    break;
+  default:
+    console.log('color is not red or blue');
+    break;
+}
 
-// Test if undefined
- if (typeof id !== 'undefined') {
-   console.log(`The ID is ${id}`);
- } else {
-   console.log('NO ID');
- }
+let day;
 
-// Ternary Operator
-console.log(id === 100 ? 'Correct' : 'Incorrect');
+switch(new Date().getDay()) {
+  case 0:
+    day = "sunday";
+    break;
+  case 1:
+    day = "Monday";
+    break;
+  case 3:
+    day = "Tuesday";
+    break;
+  case 4:
+    day = "Wednesday";
+    break;
+  case 5:
+    day = "Thursday";
+    break;  
+  case 6:
+    day = "Friday";
+    break;
+  case 7:
+    day = "Saturday";
+    break;
+}
+
+console.log(`Today is ${day}`);
