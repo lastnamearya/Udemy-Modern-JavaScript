f8acbab67d51d706d6e2819379864197eca78a8f
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-12 14:03:19 +0530

    string methods

--- app.js
+++ app.js
@@ -1,26 +1,58 @@
-const num1 = 100;
-const num2 = 50;
+const firstName = 'Wiliam';
+const lastName = "Johnson";
+const str = "Hello there my name is Brad";
+const tags = 'web design,web development,programming';
 
 let val;
 
-// Math object
+// Concatenation
+val = firstName + " " + lastName;
 
-val = Math.PI;
-val = Math.E;
-val = Math.round(2.4);
-val = Math.ceil(2.4);
-val = Math.floor(2.4);
-val = Math.sqrt(64);
-val = Math.abs(-3);
-val = Math.pow(8, 2);
-val = Math.min(1,2,3,44,55,66,-2);
-val = Math.max(1,2,3,44,55,66,-2);
-val = Math.random();  
+// Append
+val = 'Brad ';
+val += 'Traversy';
 
-// Random No between 1 & 20
-val = Math.floor(Math.random() * 20 + 1);
+// Escaping
+val = 'That\'s awesome I can\'t wait';
 
+// Length
+val = firstName.length;
 
+// Concat
+val = firstName.concat(' ', lastName);
+
+// Change Case
+val = firstName.toUpperCase();
+val = lastName.toLowerCase();
+
+// Find characters in strings
+val = firstName[1];
+
+// indexOf()
+val = firstName.indexOf('l');
+val = firstName.lastIndexOf('l');
+
+// charAt()
+val = firstName.charAt(2); 
+// Get last char
+val = firstName.charAt(firstName.length - 1);
+
+// substring()
+val = firstName.substring(0, 4);
+
+// slice()
+val = firstName.slice(0, 4);
+val = firstName.slice(-3);
+
+// Split()
+val = str.split(' ');
+val = tags.split(',');
+
+// replace()
+val = str.replace('Brad', 'Jack');
+
+// includes()
+val = str.includes('Hello');
+val = str.includes('foo');  // return with false
 
 console.log(val);
-
