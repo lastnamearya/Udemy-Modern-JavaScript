52eb7dd2d3a5739ce4f6719ad6566478fc8f1fb0
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-21 01:36:03 +0530

    global scope

--- app.js
+++ app.js
@@ -1,70 +1,25 @@
-// // Window Methods / Objects / Properties
+// Glboal Scope
+var  a = 1;
+let b = 2;
+const c = 3;
 
-// console.log(123);
-
-// window.console.log(123);
-
-// // Alert
-
-// window.alert('hello Peter Thiel');
-
-// // Prompt
-// const input = prompt();
-// alert(input);
-
-// // Confirm
-// if(confirm('Are you sure')){
-//   console.log('YES');
-// } else {
-//   console.log('No');
+// function test() {
+//   var a = 4;
+//   let b = 5;
+//   const c = 6;
+//   console.log('Function Scope: ', a, b, c);
 // }
 
-let val;
+if(true){
+  // Block Scope
+  var a = 4;
+  let b = 5;
+  const c = 6;
+  console.log('If Scope: ', a, b, c);
+}
 
-// Outer Height and Width
+for(let a = 0; a < 10; a++) {
+  console.log(`Loop: ${a}`);
+}
 
-val = window.outerHeight;
-val = window.outerWidth;
-
-// Inner Height and Width
-
-val = window.innerHeight;
-val = window.innerWidth;
-
-// Scroll Points
-
-val = window.scrollY;
-val = window.scrollX;
-
-// Location Object
-
-val = window.location;
-val = window.location.hostname; 
-val = window.location.port;
-val = window.location.href;
-val = window.location.search;
-
-// // Redirect
-
-// window.location.href = 'https://google.com';
-
-// // Reload
-
-// window.location.reload();
-
-// History Object
-
-window.history.go(-2);
-val = window.history.length;
-
-// Navigator Object
-
-val = window.navigator;
-val = window.navigator.appName;
-val = window.navigator.appVersion;
-val = window.navigator.userAgent;
-val = window.navigator.platform;
-val = window.navigator.vendor;
-val = window.navigator.language;
-
-console.log(val); 
+console.log('Global Scope: ', a, b, c);
