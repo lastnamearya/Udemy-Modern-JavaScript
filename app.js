e901254cb19ad21d0048054687c18403e027e7a0
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-28 17:38:08 +0530

    object.create syntax

--- app.js
+++ app.js
@@ -1,51 +1,26 @@
-// Constructor Name always start with a Capital Character / Name
-
-// Person constructor
-
-function Person(firstName, lastName) {
-  this.firstName = firstName;
-  this.lastName = lastName;
+const personProtoTypes = {
+  greeting: function(){
+    return `Hello there ${this.firstName} ${this.lastName}`;
+  },
+  getsMarried: function(newLastName) {
+    this.lastName = newLastName;
+  }
 }
 
-// Greeting prototype method
-Person.prototype.greeting = function() {
-  return `Hello there ${this.firstName} ${this.lastName}`;
-}
+// It will first of all create an empty object 'mary' then it will assign personProtoTypes (object) to its prototypes
+ const mary = Object.create(personProtoTypes);
+ mary.firstName = 'Mary';
+ mary.lastName = 'Williams';
+ mary.age = 30;
 
-const person1 = new Person('John', 'Doe');
+ mary.getsMarried('Thompson');
 
-console.log(person1.greeting());
+ console.log(mary.greeting());
 
-// Customer constructor
-function Customer(firstName, lastName, phone, membership) {
-  // call() is a function that allow us to call another function from somewhere else in the current context
+ const brad = Object.create(personProtoTypes, {
+   firstName: {value: 'Brad'},
+   lastName: {value: 'Traversy'},
+   age: {value: 36}
+ });
 
-  Person.call(this, firstName, lastName);
-
-  this.phone = phone;
-  this.membership = membership;
-}
-
-// ***************************************************************************************** //
-
-// Inherit Person Prototype Methods
-Customer.prototype = Object.create(Person.prototype);
-
-// Make customer.prototype return Customer()
-Customer.prototype.constructor = Customer;
-
-// ***************************************************************************************** //
-
-
-// Create a customer
-const customer1 = new Customer('Tom', 'Smith', '555-555-555', 'Standard');
-
-console.log(customer1);
-
-// Override Prototype Constructor
-Customer.prototype.greeting = function(){
-  return `Hello there ${this.firstName} ${this.lastName} welcome! to our company`;
-}
-
-// Without inheriting prototype, it'll not work. By default js will not inherit any prototype of other constructors
-console.log(customer1.greeting());
+console.log(brad);
