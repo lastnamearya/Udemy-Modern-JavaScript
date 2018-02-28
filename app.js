71333132456e4da828f588aa42b0aaaf13414327
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-28 15:11:18 +0530

    Protypes explained

--- app.js
+++ app.js
@@ -1,28 +1,57 @@
-// Person  Constructor
-function Person(name, dob) {
-  this.name = name;
+// Object.prototype
+// Person.prototype
 
-
-  // this.age = age;
-
+// Person Constructor
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
   this.birthday = new Date(dob);
-  this.calculateAge = function(){
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    // Here in Math Object(constructor), we don't need to use new keyword 
-    // Don't worry about this formula of - 1970, it's default
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
+
+  // // this is something, that we need to put inside of our prototype rather than the object constructor
+  // this.calculateAge = function(){
+  //   const diff = Date.now() - this.birthday.getTime();
+  //   const ageDate = new Date(diff);
+  //   return Math.abs(ageDate.getUTCFullYear() - 1970);
+  // }
 }
 
-// // This will give us window object
-// console.log(this);
-// this.alert(1);
+// Calculate Age Prototype
+Person.prototype.calculateAge = function(){
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
 
-// const brad = new Person('Brad', 36);
-// const john = new Person('John', 30);
+// Get Full Name Prototype
+Person.prototype.getFullName = function(){
+  // // ES5 Way
+  // return (this.firstName + " " + this.lastName);
 
-// console.log(john.age);
+  // ES6 Way using template literals
+  return `${this.firstName} ${this.lastName}`;
+}
 
-const brad = new Person('Brad', '9-10-1981');
-console.log(brad.calculateAge());
+// Get Married Prototype
+Person.prototype.getsMarried = function(newLastName){
+  this.lastName = newLastName;
+}
+
+const john = new Person('John', 'Doe', '8-12-90');
+const mary = new Person('Mary', 'Johnson', 'March 20 1978');
+
+console.log(mary);
+
+console.log(john.calculateAge());
+
+console.log(mary.getFullName());
+
+mary.getsMarried('Smith');
+
+console.log(mary.getFullName());
+
+// Access Prototype Properties
+console.log(mary.hasOwnProperty('firstName'));
+
+// We get false value, because it's under the prototype not in the object's properties
+console.log(mary.hasOwnProperty('getFullName'));
+
