587babb7325bd3d700ec3354acf07b4e35763b08
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-28 18:13:09 +0530

    ES6 classes

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-const personProtoTypes = {
-  greeting: function(){
-    return `Hello there ${this.firstName} ${this.lastName}`;
-  },
-  getsMarried: function(newLastName) {
-    this.lastName = newLastName;
-  }
-}
+ // ES6 Classes
+ class Person {
+   constructor(firstName, lastName, dob) {
+     this.firstName = firstName;
+     this.lastName = lastName;
+     this.birthday = new Date(dob);
+   }
 
-// It will first of all create an empty object 'mary' then it will assign personProtoTypes (object) to its prototypes
- const mary = Object.create(personProtoTypes);
- mary.firstName = 'Mary';
- mary.lastName = 'Williams';
- mary.age = 30;
+   greeting() {
+     return `Hello there ${this.firstName} ${this.lastName}`; 
+   }
 
- mary.getsMarried('Thompson');
+   calculateAge() {
+     const diff = Date.now() - this.birthday.getTime();
+     const ageDate = new Date(diff);
+     return Math.abs(ageDate.getUTCFullYear() - 1970);
+   }
 
- console.log(mary.greeting());
+   getsMarried(newLastName) {
+     this.lastName = newLastName;
+   }
 
- const brad = Object.create(personProtoTypes, {
-   firstName: {value: 'Brad'},
-   lastName: {value: 'Traversy'},
-   age: {value: 36}
- });
+  // Static Methods, we can't use static methods on the object instances that we create using the ES6 classes or object constructor
+  // If you're not using something like this keyword in your methods then static method is the best way to do the task
+   static addNumbers(x, y) {
+     return x + y;
+   }
 
-console.log(brad);
+ }
+
+const mary = new Person('Mary', 'Williams', '11-13-1980');
+
+console.log(mary);
+
+mary.getsMarried('Thompson');
+
+console.log(mary.greeting());
+
+console.log(mary.calculateAge());
+
+console.log(Person.addNumbers(1,2));
