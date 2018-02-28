10ff807b3630dc216f042599754e32e5cf06658b
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-28 12:53:33 +0530

    document object

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-// Glboal Scope
-var  a = 1;
-let b = 2;
-const c = 3;
+let val;
 
-// function test() {
-//   var a = 4;
-//   let b = 5;
-//   const c = 6;
-//   console.log('Function Scope: ', a, b, c);
-// }
+val = document;
+val = document.all;
+val = document.all[3];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
 
-if(true){
-  // Block Scope
-  var a = 4;
-  let b = 5;
-  const c = 6;
-  console.log('If Scope: ', a, b, c);
-}
+val = document.forms;
 
-for(let a = 0; a < 10; a++) {
-  console.log(`Loop: ${a}`);
-}
+// Select First Form
+val = document.forms[0];
 
-console.log('Global Scope: ', a, b, c);
+// Get Stuff from Forms
+val = document.forms[0].id;
+
+// Forms have methods like get and post
+val = document.forms[0].method;
+
+// *********************************************************************** //
+
+// We can find links in our document
+val = document.links;
+val = document.links[0];
+val = document.links[0].id;
+val = document.links[0].className;
+val = document.links[0].classList[0];
+
+val = document.images;
+
+val = document.scripts;
+val = document.scripts[2].getAttribute('src');
+
+console.log(val);
