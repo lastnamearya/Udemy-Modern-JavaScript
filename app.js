b6c2cd39ce8eb7a567d3d8a69c3f35a466e4d842
lastnamearya <arya.jigyasu6815@gmail.com>
2018-02-28 15:52:41 +0530

    built-in Constructors

--- app.js
+++ app.js
@@ -1,57 +1,58 @@
-// Object.prototype
-// Person.prototype
+// All these constructor methods will slow your scipt execution time, ignore them as possible but you need to keep them in your mind.
 
-// Person Constructor
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.birthday = new Date(dob);
+// String
 
-  // // this is something, that we need to put inside of our prototype rather than the object constructor
-  // this.calculateAge = function(){
-  //   const diff = Date.now() - this.birthday.getTime();
-  //   const ageDate = new Date(diff);
-  //   return Math.abs(ageDate.getUTCFullYear() - 1970);
-  // }
+// Primitive Data Type
+const name1 = 'Jeff';
+
+// Reference ~ Object Constructor
+const name2 = new String('Jeff');
+
+// // We can add properties to reference data types (string in this case) whenever we want to
+// name2.foo = 'bar';
+// console.log(name2);
+
+console.log(typeof name2);
+
+// We can create any data in primitive and reference form, whatever we want to
+const num1 = 5;
+const num2 = new Number(5);
+
+console.log(num1);
+console.log(typeof(num2));
+
+// Boolean
+const bool1 = true;
+const bool2 = new Boolean(true);
+
+console.log(typeof bool2);
+
+// Functions
+const getSum1 = function(x, y){
+  return x + y;
 }
 
-// Calculate Age Prototype
-Person.prototype.calculateAge = function(){
-    const diff = Date.now() - this.birthday.getTime();
-    const ageDate = new Date(diff);
-    return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
+// Constructor Function
+const getSum2 = new Function('x','y', 'return x + y');
 
-// Get Full Name Prototype
-Person.prototype.getFullName = function(){
-  // // ES5 Way
-  // return (this.firstName + " " + this.lastName);
+console.log(getSum2(1,1));
 
-  // ES6 Way using template literals
-  return `${this.firstName} ${this.lastName}`;
-}
+// Object Constructor
+const john1 = {name: "John"};
+const john2 = new Object({name: "John"});
+console.log(john2);
 
-// Get Married Prototype
-Person.prototype.getsMarried = function(newLastName){
-  this.lastName = newLastName;
-}
+// Arrays
+const arr1 = [1,2,3,4];
+const arr2 = new Array(1,2,3,4);
 
-const john = new Person('John', 'Doe', '8-12-90');
-const mary = new Person('Mary', 'Johnson', 'March 20 1978');
+console.log(arr2);
 
-console.log(mary);
+// Regular Expressions Constructor
+// In this expression we're looking for a word character that appears more than one time.
+const re1 = /\w+/;
+// When we use RegExp then we need to escape our backslash character uing one more \
+const re2 = new RegExp('\\w+');
 
-console.log(john.calculateAge());
-
-console.log(mary.getFullName());
-
-mary.getsMarried('Smith');
-
-console.log(mary.getFullName());
-
-// Access Prototype Properties
-console.log(mary.hasOwnProperty('firstName'));
-
-// We get false value, because it's under the prototype not in the object's properties
-console.log(mary.hasOwnProperty('getFullName'));
-
+console.log(re1);
+console.log(re2);
