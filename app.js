219e918664bde508049ea93dc5f29259003f5cee
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-10 00:32:51 +0530

    previous onreadystatechange method

--- app.js
+++ app.js
@@ -8,11 +8,17 @@ function loadData() {
   // Create the callback
 
   // onload is pretty new, before that we do onreadystatechange, then we check to make sure that readystate is at 4, now it will check to the readystate that must be equal to 4
-  xhr.onload = function() {
-   if(this.status === 200) {
-    console.log(this.responseText);
-   } 
-  }
+  // xhr.onload = function() {
+  //  if(this.status === 200) {
+  //   console.log(this.responseText);
+  //  } 
+  // }
+
+  xhr.onreadystatechange = function() {
+    if(this.status === 200 && this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    }
 
   // Open
   xhr.open('GET', 'data.txt', true);
