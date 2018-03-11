e4381d8833194f34371f92d3f76e82bc104cac19
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-12 02:57:35 +0530

    await will make our asyn function asynchronous

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 async function myFunc() {
-  return 'Hello';
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Hello'), 1000);
+  });
+
+  const res = await promise;  // Wait until promise is resolved. 
+  return res;
 }
 
 myFunc()
