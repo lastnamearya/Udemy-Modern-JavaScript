3e4faed8de92ec517ee2753a1a0cb403664e868b
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-12 02:52:23 +0530

    asyn make our function return promises

--- app.js
+++ app.js
@@ -1,29 +1,6 @@
-// // One line function doesn't need braces
-// const sayHello = () => console.log('hello');
+async function myFunc() {
+  return 'Hello';
+}
 
-// // Don't need return keyword in single line function
-// const sayHello = () => 'Hello';
-
-// // Return Object Literal
-// const sayHello = () => ({ msg: 'Hello'});
-
-// console.log(sayHello());
-
-// // Parameters
-
-// // // Single Parameters, even don't need parenthesis
-// const sayHello = name => console.log(`Hello ${name}`);
-
-// // More than one Parameters
-// const sayHello2 = (firstName, lastName) => console.log(`Hello ${firstName} ${lastName}`);
-
-// sayHello('Jigyasu');
-// sayHello2('Jigyasu', 'Arya');
-
-// Arrow functions in callback
-
-const users = ['Nathan', 'John', 'Willian'];
-
-const nameLengths = users.map(name => name.length);
-
-console.log(nameLengths);
+myFunc()
+  .then(res => console.log(res));
