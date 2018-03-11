10704abd890e126b6c7cd5413e97114cb7b2fe72
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-11 05:33:50 +0530

    FetchAPI get text

--- app.js
+++ app.js
@@ -1,36 +1,14 @@
-const posts = [
-  {title: 'Post One', body: 'This is post one'},
-  {title: 'Post Two', body: 'This is post two'}
-];
+document.getElementById('button1').addEventListener('click', getText);
 
-function createPost(post) {
-  return new Promise(function(resolve, reject){
-    setTimeout(function() {
-      posts.push(post);
-
-      const error = true;
-
-      if(!error) {
-        resolve(); 
-      } else {
-        reject('Error: Something went wrong');
-      }
-      resolve();
-    }, 2000);
-  });
-  
+function getText() {
+  fetch('text.txt')
+  .then(function(res){
+    return res.text();    // If we want JSON then we type res.JSON()
+  })
+  .then(function(data){
+    console.log(data);
+  })
+  .catch(function(err){
+    console.log(err);
+  })
 }
-
-function getPosts() {
-  setTimeout(function() {
-    let output = '';
-    posts.forEach(function(post){
-      output += `<li>${post.title}</li>`;
-    });
-    document.body.innerHTML = output;
-  }, 1000);
-}
-
-createPost({title: 'Post Three', body: 'This is post three'}).then(getPosts).catch(function(err) {
-  console.log(err);
-});
