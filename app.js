ccdea44ae13f9d16a05583f02f201c4a462714cb
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-11 05:58:23 +0530

    FetchAPI External(github API) Data

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ document.getElementById('button1').addEventListener('click', getText);
 
 document.getElementById('button2').addEventListener('click', getJSON);
 
+document.getElementById('button3').addEventListener('click', getExternal);
+
+// ******************************************************************************* //
+
 // Get local file data
 function getText() {
   fetch('text.txt')
@@ -28,6 +32,25 @@ function getJSON() {
     let output = "";
     data.forEach(function(post){
       output += `<li>${post.title}</li>`;
+    });
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
+
+// Get from Local API
+function getExternal() {
+  fetch('https://api.github.com/users')
+  .then(function(res){
+    return res.json();    
+  })
+  .then(function(data){
+    console.log(data);
+    let output = "";
+    data.forEach(function(user){
+      output += `<li>${user.login}</li>`;
     });
     document.getElementById('output').innerHTML = output;
   })
