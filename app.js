1a06e0db44ffa426929f28e1b36d6be592df73e3
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-11 06:23:23 +0530

    arroa --returning object literal

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 // // One line function doesn't need braces
 // const sayHello = () => console.log('hello');
 
-// Don't need return keyword in single line function
-const sayHello = () => 'Hello';
+// // Don't need return keyword in single line function
+// const sayHello = () => 'Hello';
 
+// Return Object Literal
+const sayHello = () => ({ msg: 'Hello'});
 
 console.log(sayHello());
