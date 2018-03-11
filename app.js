160240aa54e8715844dbd9a0c1ffa10b3fe67600
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-11 05:48:37 +0530

    FetchAPI JSON Data

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 document.getElementById('button1').addEventListener('click', getText);
 
+document.getElementById('button2').addEventListener('click', getJSON);
+
+// Get local file data
 function getText() {
   fetch('text.txt')
   .then(function(res){
@@ -7,6 +10,26 @@ function getText() {
   })
   .then(function(data){
     console.log(data);
+    document.getElementById('output').innerHTML = data;
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+}
+
+// Get local JSON Data
+function getJSON() {
+  fetch('posts.json')
+  .then(function(res){
+    return res.json();    
+  })
+  .then(function(data){
+    console.log(data);
+    let output = "";
+    data.forEach(function(post){
+      output += `<li>${post.title}</li>`;
+    });
+    document.getElementById('output').innerHTML = output;
   })
   .catch(function(err){
     console.log(err);
