05e0963b96f07a45fa843d94f988cc096524342a
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-11 06:13:19 +0530

    arrow --one line doesn't need braces

--- app.js
+++ app.js
@@ -1,60 +1,6 @@
-document.getElementById('button1').addEventListener('click', getText);
+// One line function doesn't need braces
+const sayHello = () => console.log('hello');
 
-document.getElementById('button2').addEventListener('click', getJSON);
 
-document.getElementById('button3').addEventListener('click', getExternal);
 
-// ******************************************************************************* //
-
-// Get local file data
-function getText() {
-  fetch('text.txt')
-  .then(function(res){
-    return res.text();    // If we want JSON then we type res.JSON()
-  })
-  .then(function(data){
-    console.log(data);
-    document.getElementById('output').innerHTML = data;
-  })
-  .catch(function(err){
-    console.log(err);
-  })
-}
-
-// Get local JSON Data
-function getJSON() {
-  fetch('posts.json')
-  .then(function(res){
-    return res.json();    
-  })
-  .then(function(data){
-    console.log(data);
-    let output = "";
-    data.forEach(function(post){
-      output += `<li>${post.title}</li>`;
-    });
-    document.getElementById('output').innerHTML = output;
-  })
-  .catch(function(err){
-    console.log(err);
-  })
-}
-
-// Get from Local API
-function getExternal() {
-  fetch('https://api.github.com/users')
-  .then(function(res){
-    return res.json();    
-  })
-  .then(function(data){
-    console.log(data);
-    let output = "";
-    data.forEach(function(user){
-      output += `<li>${user.login}</li>`;
-    });
-    document.getElementById('output').innerHTML = output;
-  })
-  .catch(function(err){
-    console.log(err);
-  })
-}
+sayHello();
