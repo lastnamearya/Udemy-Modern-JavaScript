3fe4c5a95ee4c1dc67abdf904a6a7dc38bae1a47
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-12 03:00:51 +0530

    handling error with async & await

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ async function myFunc() {
     setTimeout(() => resolve('Hello'), 1000);
   });
 
-  const res = await promise;  // Wait until promise is resolved. 
-  return res;
+  const error = true;
+
+  if(!error) {
+    const res = await promise;  // Wait until promise is resolved. 
+    return res;
+  } else {
+    await Promise.reject(new Error('Something went wrong'));
+  }
 }
 
 myFunc()
-  .then(res => console.log(res));
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
