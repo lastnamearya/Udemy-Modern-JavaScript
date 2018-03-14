1e70ca9be40ce80fdd3b3f967b60fc0793bf21c8
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-15 00:25:47 +0530

    Reference Error with catch()

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-// async function myFunc() {
-
-//   const promise = new Promise((resolve, reject) => {
-//     setTimeout(() => resolve('Hello'), 1000);
-//   });
-
-//   const error = true;
-
-//   if(!error) {
-//     const res = await promise;  // Wait until promise is resolved. 
-//     return res;
-//   } else {
-//     await Promise.reject(new Error('Something went wrong'));
-//   }
-// }
-
-// myFunc()
-//   .then(res => console.log(res))
-//   .catch(err => console.log(err));
-
-async function getUsers() {
-  // await response of fetch call
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-
-  // only proceed once its resolved
-  const data = await response.json();
-
-  // only proceed once second promise is resolved
-  return data;
+try {
+  // Produce a Reference Error
+  myFunction();
+} catch(e) {
+  console.log(e);
 }
 
-getUsers().then(users => console.log(users));
+console.log('Program continues....');
