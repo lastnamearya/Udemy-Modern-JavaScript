2dbfe52e1c9481b80dd359ee19e44ec38cb2492d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-15 01:10:01 +0530

    that's the exec( ) function

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ console.log(re.source);
 
 // exec() - Return result in an array if it found the match or it'll return null if it not able to found the match
 
-const result = re.exec('hellosdfsdfsd world');
+const result = re.exec('hello world');
 
 console.log(result);
+
+// We can find sub-values of the result 
+
+console.log(result[0]);
+console.log(result.index);
+// search string
+console.log(result.input);
