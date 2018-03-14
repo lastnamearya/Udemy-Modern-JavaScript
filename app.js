71d96f46436a50de5236effe8c624b189e3e8715
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-15 01:07:16 +0530

    exec() 2nd Example

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ console.log(re.source);
 
 // exec() - Return result in an array if it found the match or it'll return null if it not able to found the match
 
-const result = re.exec('brad hello world');
+const result = re.exec('hellosdfsdfsd world');
 
 console.log(result);
