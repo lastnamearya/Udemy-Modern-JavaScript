5f67b2943674a7bb30dff691c5a55d81cd9c2a9e
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-15 01:14:12 +0530

    i makes our body of RegEx case-insensitive

--- app.js
+++ app.js
@@ -2,20 +2,27 @@ let re;
 
 // It's a regular Expression that can be identified by 2 forward slashes and hello is the body of the regular expression.
 
-re = /hello/;
+re = /hello/i;
 
-console.log(re);
-console.log(re.source);
+// console.log(re);
+// console.log(re.source);
 
-// exec() - Return result in an array if it found the match or it'll return null if it not able to found the match
+// // exec() - Return result in an array if it found the match or it'll return null if it not able to found the match
 
-const result = re.exec('hello world');
+// const result = re.exec('hello world');
+
+// console.log(result);
+
+// // We can find sub-values of the result 
+
+// console.log(result[0]);
+// console.log(result.index);
+// // search string
+// console.log(result.input);
+
+// ******************************************************************************** //
+
+// test() - Returns true or false
+const result = re.test('Hello');
 
 console.log(result);
-
-// We can find sub-values of the result 
-
-console.log(result[0]);
-console.log(result.index);
-// search string
-console.log(result.input);
