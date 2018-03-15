96183fa29b4052355374713b0c1036c4466fb04d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-16 00:59:26 +0530

    Revealing Module Pattern

--- app.js
+++ app.js
@@ -9,27 +9,53 @@
 //   }
 // })();
 
-// STANDARD MODULE PATTERN
-const UICtrl = (function() {
-  let text = 'Hello World';
+// // STANDARD MODULE PATTERN
+// const UICtrl = (function() {
+//   let text = 'Hello World';
 
-  const changeText = function() {
-    const element = document.querySelector('h1');
-    element.textContent = text;
+//   const changeText = function() {
+//     const element = document.querySelector('h1');
+//     element.textContent = text;
+//   }
+
+//   return {
+//     callChangeText: function() {
+//       changeText();
+//       console.log(text);
+//     }
+//   }
+// })();
+
+// UICtrl.callChangeText();
+
+// // Can't access Private Data ~ functions
+// UICtrl.changeText();
+
+// // Can't access Private Data ~ Variables 
+// console.log(UICtrl.text);
+
+// ************************************************************************************************ //
+
+// Revealing Module Pattern
+const ItemCtrl = (function() {
+  // This can be thought as our State
+  let data = [];
+
+  function add(item) {
+    data.push(item);
+    console.log('Item Added...');
+  }
+
+  function get(id) {
+    return data.find(item => {
+      return item.id === id;
+    })
   }
 
   return {
-    callChangeText: function() {
-      changeText();
-      console.log(text);
-    }
+    add: add,
+    get: get
   }
 })();
 
-UICtrl.callChangeText();
-
-// Can't access Private Data ~ functions
-UICtrl.changeText();
-
-// Can't access Private Data ~ Variables
-console.log(UICtrl.text);
+ItemCtrl.add({id: 1, name: 'John'});
