04c7d77b51f66c57bcd7a051ccf24a8ed38542a4
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-16 00:47:03 +0530

    Standard Module Pattern

--- app.js
+++ app.js
@@ -1,56 +1,35 @@
-// Maps = Key-value Pairs - can use ANY type as key or value
+// // Basic Structure
 
-const map1 = new Map();
+// (function() {
+//   // Declare Private vars and functions 
 
-// Set some keys
+//   return {
+//     // Declare Public Varaibles and Functions
 
-const key1 = "Some string",
-      key2 = {},
-      key3 = function() {};
+//   }
+// })();
 
-// Set map values by key
-map1.set(key1, 'Value of key1');
-map1.set(key2, 'Value of key2');
-map1.set(key3, 'Value of key3');
+// STANDARD MODULE PATTERN
+const UICtrl = (function() {
+  let text = 'Hello World';
 
-// Get Values by key
-console.log(map1.get(key1), map1.get(key2), map1.get(key3));
+  const changeText = function() {
+    const element = document.querySelector('h1');
+    element.textContent = text;
+  }
 
-// Count values
-console.log(map1.size);
+  return {
+    callChangeText: function() {
+      changeText();
+      console.log(text);
+    }
+  }
+})();
 
-// Iterating through Maps
+UICtrl.callChangeText();
 
-// Loop using for...of to get keys and values
-for(let [key, value] of map1) {
-  console.log(`${key} = ${value}`);
-}
+// Can't access Private Data ~ functions
+UICtrl.changeText();
 
-// Iterating only through the Keys
-for(let key of map1.keys()) {
-  console.log(key);
-}
-
-// Iterating only through the values
-for(let value of map1.values()) {
-  console.log(value);
-}
-
-// Loop with forEach
-map1.forEach(function(value, key) {
-  console.log(`${key} = ${value}`);
-});
-
-// Convert to Arrays
-
-// Create an array of key value pairs
-const keyValArr = Array.from(map1);
-console.log(keyValArr);
-
-// Create an array of just values from map
-const valArr = Array.from(map1.values());
-console.log(valArr);
-
-// Create an array of just values from map
-const keyArr = Array.from(map1.keys());
-console.log(keyArr);
+// Can't access Private Data ~ Variables
+console.log(UICtrl.text);
