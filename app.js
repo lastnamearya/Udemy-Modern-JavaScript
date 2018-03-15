3e1050ed784eb9af34a11d302c2724ce914af84b
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-16 02:41:13 +0530

    Observer Pattern

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
-// Factory Pattern
-function MemberFactory() {
-  this.createMember = function(name, type) {
-
-    let member;
-
-    if(type === 'simple') {
-      member = new SimpleMembership(name);
-    } else if (type === 'standard') {
-      member = new StandardMembership(name);
-    } else if (type === "super") {
-      member = new SuperMembership(name);
-    }
-
-    member.type = type;
-
-    member.define = function() {
-      console.log(`${this.name} (${this.type}): ${this.cost}`);
-    }
-
-    return member;
-  }  
+function EventObserver() {
+  this.observers = [];
 }
 
-const SimpleMembership = function(name) {
-  this.name = name;
-  this.cost = '$5';
+EventObserver.prototype = {
+  subscribe: function(fn) {
+    this.observers.push(fn);
+    console.log(`You are now subscribed to ${fn.name}`);
+  },
+  unsubscribe: function(fn) {
+    // Filter out from the list whatever matches the callback function. If there is no match, the callback gets to stay on the list. The filters returns a new list and reassigns the list of observers.
+    this.observers = this.observers.filter(function(item) {
+      if(item !== fn) {
+        return item;
+      }
+    });
+    console.log(`You are now unsubscribed from ${fn.name}`);
+  },
+  fire: function() {
+    this.observers.forEach(function(item) {
+       item.call();
+    })
+  }
 }
 
-const StandardMembership = function(name) {
-  this.name = name;
-  this.cost = '$15';
-}
+const click = new EventObserver();
 
-const SuperMembership = function(name) {
-  this.name = name;
-  this.cost = '$25';
-}
-
-const members = [];
-const factory = new MemberFactory();
-
-members.push(factory.createMember('John Doe', 'simple'));
-members.push(factory.createMember('Chris Jackson', 'super'));
-members.push(factory.createMember('Janice Williams', 'simple'));
-members.push(factory.createMember('Tom Smith', 'standard'));
-
-console.log(members);
-
-members.forEach(function(member) {
-  member.define();
+// Event Listeners
+document.querySelector('.sub-ms').addEventListener('click', function() {
+  click.subscribe(getCurrentMilliSeconds);
 });
+
+document.querySelector('.unsub-ms').addEventListener('click', function() {
+  click.unsubscribe(getCurrentMilliSeconds);
+});
+
+document.querySelector('.sub-s').addEventListener('click', function() {
+  click.subscribe(getCurrentSeconds);
+});
+
+document.querySelector('.unsub-s').addEventListener('click', function() {
+  click.unsubscribe(getCurrentSeconds);
+});
+
+document.querySelector('.fire').addEventListener('click', function() {
+  click.fire();
+});
+
+// Click Handler
+const getCurrentMilliSeconds  = function() {
+  console.log(`Current Milliseconds: ${new Date().getMilliseconds()}`);
+}
+
+const getCurrentSeconds  = function() {
+  console.log(`Current Seconds: ${new Date().getSeconds()}`);
+}
