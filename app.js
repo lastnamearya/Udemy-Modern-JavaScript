be29b80ee641bd8794158811eaddcbd2b754ecc6
lastnamearya <arya.jigyasu6815@gmail.com>
2018-03-15 16:16:49 +0530

    Generators

--- app.js
+++ app.js
@@ -1,23 +1,57 @@
 // Iterator Example
-function nameIterator(names) {
-  let nextIndex = 0;
+// function nameIterator(names) {
+//   let nextIndex = 0;
 
-  return {
-    next: function() {
-      return nextIndex < names.length ?
-      { value: names[nextIndex++], done: false } :
-      { done: true }
-    }
+//   return {
+//     next: function() {
+//       return nextIndex < names.length ?
+//       { value: names[nextIndex++], done: false } :
+//       { done: true }
+//     }
+//   }
+// }
+
+// // Create an array of names
+// const namesArr = ['Jack', 'Jill', 'John'];
+
+// // Init iterator and pass in the names array
+// const names = nameIterator(namesArr);
+
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next().value);
+// console.log(names.next());
+
+// **************************************************************************************************** //
+
+// Generator Example
+function* sayNames() {
+  yield 'Jack';
+  yield 'Jill';
+  yield 'John';
+}
+
+const name = sayNames();
+
+console.log(name.next().value);
+console.log(name.next().value);
+console.log(name.next().value);
+console.log(name.next());
+
+// ID Creator
+function* createIds() {
+  let index = 1;
+
+  while(true) {
+    yield index++;
   }
 }
 
-// Create an array of names
-const namesArr = ['Jack', 'Jill', 'John'];
+const gen = createIds();
 
-// Init iterator and pass in the names array
-const names = nameIterator(namesArr);
-
-console.log(names.next().value);
-console.log(names.next().value);
-console.log(names.next().value);
-console.log(names.next());
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
